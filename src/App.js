d4f1b9702ebc2a61e7754da9cc785c8118e939f0
brunokian <brunokian@hotmail.com>
2023-05-05 13:06:16 -0300

    time generator

--- src/App.js
+++ src/App.js
@@ -1,33 +1,52 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import chordsList from './Datas';
 
 function App() {
   const [chord, setChord] = useState('')
-  const [lastChord, setLastChord] = useState('')
+  const [hasStarted, setHasStarted] = useState(false)
+  const [intervalId, setIntervalId] = useState(null)
 
   const randomChords = () => {
     const index = Math.floor(Math.random() * chordsList.length)
     // console.log(chordsList[index]);
-    return chordsList[index]
+    const newChord = chordsList[index]
+    if (newChord !== chord) {
+      setChord(newChord)
+      return newChord
+    } else {
+      randomChords()
+    }
   }
 
   const chordsGenerator = () => {
     // console.log(randomChords());
-    const newChord = randomChords()
-    if (newChord !== chord) {
-      setChord(newChord)
-    } else {
-      chordsGenerator()
+    return randomChords()
+  }
+
+  useEffect(() => {
+    if (hasStarted) {
+      const id = setInterval(() => {
+        chordsGenerator()
+      }, 1000)
+      setIntervalId(id)
     }
-    
+    return () => clearInterval(intervalId)
+  }, [hasStarted])
+
+  const handleClick = () => {
+    setHasStarted(!hasStarted)
+    if (intervalId) {
+      clearInterval(intervalId)
+      setIntervalId(null)
+    }
   }
 
   return (
     <div>
       <h1>{chord}</h1>
-      <button onClick={ chordsGenerator}>
-        START
+      <button onClick={handleClick}>
+        {hasStarted ? 'STOP': 'START'}
       </button>
     </div>
   );
