6c41c8f2c2dc423e232debfb58693ff07e1b7201
brunokian <brunokian@hotmail.com>
2023-05-05 11:33:03 -0300

    logica para gerar triade

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import chordsList from './Datas';
 
 function App() {
+  const [chord, setChord] = useState('')
+
+  const randomChords = () => {
+    const index = Math.floor(Math.random() * chordsList.length)
+    // console.log(chordsList[index]);
+    return chordsList[index]
+  }
+
+  const chordsGenerator = () => {
+    // console.log(randomChords());
+    setChord(randomChords())
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{chord}</h1>
+      <button onClick={ chordsGenerator}>
+        START
+      </button>
     </div>
   );
 }
---
+++ src/Datas.js
@@ -0,0 +1,3 @@
+let chordsList = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+
+export default chordsList
