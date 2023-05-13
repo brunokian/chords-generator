b9cd94577577f494ae33963cbde5064e4b873455
Wes <teixeiraw42@hotmail.com>
2023-05-12 22:47:18 -0300

    Adding keys

--- src/Components/ConfigurationPanel.js
+++ src/Components/ConfigurationPanel.js
@@ -1,43 +1,32 @@
-import { useEffect, useState } from 'react';
 import SelectMenu from './Inputs/SelectMenu';
+import Toggle from './Inputs/Toggle';
+import Slider from './Inputs/Slider';
+import utils from './Utils';
 
 
 export default function ConfigurationPanel(props) {
-  return (
-    <div className='w-full h-full flex flex-col'>
-        <div className='m-auto'>
-            {props.children}
-        </div>
-        <div className='bg-stone-700 rounded-lg p-2 my-4 mx-auto w-[50%]'>
-            <div className='text-2xl py-4 px-12'>
-                Configuration
+    const keys = utils.notes.reduce((a, v) => ({ ...a, [v]: v}), {"Random":""})
+
+    return (
+        <div className='w-full h-full flex flex-col'>
+            <div className='m-auto'>
+                {props.children}
             </div>
-            <div className='bg-stone-600 rounded p-2'>
-                <div>
-                    <label for="disabled-range" className="block mb-2 text-lg">Chord changes speed</label>
-                    <input id="disabled-range" type="range" value="10" className="w-full h-3 bg-stone-200 rounded-lg appearance-none cursor-pointer"/>
-                </div>
-                <div>
-                    <label for="toggleB" className="flex items-center cursor-pointer my-3">
-                        <div className="relative">
-                            <input type="checkbox" id="toggleB" className="sr-only" />
-                            <div className="block bg-stone-400 w-14 h-8 rounded-full"></div>
-                            <div className="dot absolute left-1 top-1 bg-slate-50 w-6 h-6 rounded-full transition"></div>
-                        </div>
-                        <div className="ml-3 text-lg">Sharp chords</div>
-                    </label>
-                    <label for="toggleB" className="flex items-center cursor-pointer my-3">
-                        <div className="relative">
-                            <input type="checkbox" id="toggleB" className="sr-only" />
-                            <div className="block bg-stone-400 w-14 h-8 rounded-full"></div>
-                            <div className="dot absolute left-1 top-1 bg-slate-50 w-6 h-6 rounded-full transition"></div>
-                        </div>
-                        <div className="ml-3 text-lg">Flat chords</div>
-                    </label>
-                    <SelectMenu ></SelectMenu>
+            <div className='bg-stone-700 rounded-lg p-2 my-4 mx-auto lg:w-[30%] md:w-[55%] w-full'>
+                <div className='rounded md:p-2  text-sm'>
+                    <Slider>Chord changes speed</Slider>
+                    <Toggle>Sharp Chords</Toggle>
+                    <Toggle>Flat Chords</Toggle>
+                    <div className='bg-stone-600 p-2 rounded'>
+                        <SelectMenu title={"Choose the Key"} options={keys}/>
+                        <Slider>Key change speed</Slider>
+                        <SelectMenu title={"Choose the chords scale"} options={{
+                            "Bepop": ["1", "7", "b7", "6", "5", "4", "3", "2"],
+                            "Natural": ["1", "2", "b3", "4", "5", "b6", "b7"]
+                        }}/>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  );
+    );
 };
---
+++ src/Components/Utils.js
@@ -0,0 +1,24 @@
+
+const utils = {
+    notes: [
+        "A",
+        "A#",
+        "Ab",
+        "B",
+        "Bb",
+        "C",
+        "C#",
+        "D",
+        "D#",
+        "Db",
+        "E",
+        "Eb",
+        "F",
+        "F#",
+        "G",
+        "G#",
+        "Gb",
+    ]
+}
+
+export default utils;
