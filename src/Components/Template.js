4ae18274bd0be9f4bb16767bb5edc9f1f951f4ba
Wes <teixeiraw42@hotmail.com>
2023-05-12 22:46:50 -0300

    cleaning

--- src/Components/Template.js
+++ src/Components/Template.js
@@ -1,11 +1,8 @@
-import { useEffect, useState } from 'react';
 import HistoryPanel from './HistoryPanel';
 import ConfigurationPanel from './ConfigurationPanel';
 
 
 export default function Template(props) {
-	const chordList = ['A', 'B', 'C', 'D'];
-
 	return (
 		<div className='bg-stone-800 text-slate-50 antialiased'>
 			<div className='h-screen w-screen flex items-center'>
