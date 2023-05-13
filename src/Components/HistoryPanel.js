1c3938858f54928d5d64a19762e71e233df3bbc3
Wes <teixeiraw42@hotmail.com>
2023-05-12 22:46:26 -0300

    Adjusting history panel

--- src/Components/HistoryPanel.js
+++ src/Components/HistoryPanel.js
@@ -1,17 +1,15 @@
-import { useEffect, useState } from 'react';
-
 
 export default function HistoryPanel() {
 	const chordList = ['A', 'B', 'C', 'D'];
 
 	return (
-		<div className='h-full p-4'>
-			<div className='h-full bg-stone-700 rounded-lg p-2'>
-				<div className='text-2xl p-4'>
+		<div className='h-full md:p-4'>
+			<div className='h-full bg-stone-700 rounded-lg md:p-1'>
+				<div className='text-md p-1'>
 					History
 				</div>
-				<div className='rounded bg-stone-600 w-full px-4 py-2 '>
-					{chordList.map((chord) => <div className='text-center py-1 font-medium'> {chord} </div>)}
+				<div className='rounded bg-stone-600 py-1'>
+					{chordList.map((chord, index) => <div key={index} className='text-center py-1'> {chord} </div>)}
 				</div>
 			</div>
 		</div>
