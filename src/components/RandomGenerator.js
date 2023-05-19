2d4bd6f693f30b05f15a5d3ce37e0a46db2c69c8
brunokian <brunokian@hotmail.com>
2023-05-19 17:53:09 -0300

    adicionando chords variations

--- src/components/RandomGenerator.js
+++ src/components/RandomGenerator.js
@@ -3,22 +3,34 @@ import { naturalChords, flatChords, sharpChords } from '../Datas';
 
 function RandomGenerator() {
   const [chord, setChord] = useState('C')
-  const [selectChords, setSelectChords] = useState('')
+  const [selectChords, setSelectChords] = useState('all')
   const [selectTimer, setSelectTimer] = useState(2)
   const [hasStarted, setHasStarted] = useState(false)
   const [intervalId, setIntervalId] = useState(null)
+  const [hasVariation, setHasVariation] = useState(true)
 
   const randomChords = (arrayChords) => {
     const index = Math.floor(Math.random() * arrayChords.length)
+    let variation = Math.floor(Math.random() * 2)
     // console.log(arrayChords);
-    const newChord = arrayChords[index]
+    let newChord = arrayChords[index]
+    if (hasVariation && Math.random() > 0.5) {
+      newChord = newChord + 'm'
+    }
+    if (hasVariation && Math.random() > 0.5) {
+      newChord = newChord + '7'
+    }
+    if (hasVariation && Math.random() > 0.5 && newChord.includes('7')) {
+      newChord = newChord + 'M'
+    }
+
     setChord(newChord)
     // console.log(newChord);
-    if (newChord !== chord) {
-      return newChord
-    } else {
-      return arrayChords[index+1]
-    }
+    // if (newChord !== chord) {
+    //   return newChord
+    // } else {
+    //   return arrayChords[index+1]
+    // }
   }
 
   const chordsGenerator = () => {
@@ -61,6 +73,10 @@ function RandomGenerator() {
     }
   }
 
+  const handleCheckboxChange = (event) => {
+    setHasVariation(event.target.checked);
+  };
+
   return (
     <div>
       <h1>{chord}</h1>
@@ -74,6 +90,9 @@ function RandomGenerator() {
         <option value='sharp'>sharp</option>
         <option value='all'>all</option>
       </select>
+
+      <label for='variations'>Variations</label>
+      <input type='checkbox' name='variations' checked={hasVariation} onChange={handleCheckboxChange}/>
 
       <label for='timer'>Timer</label>
       <select name='timer' value={selectTimer} onChange={(event) => handleSelect(event, 'timer')}>
