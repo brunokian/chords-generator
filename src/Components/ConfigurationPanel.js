0385c25237b4da1ea2020423f6026347763dfb30
Wes <teixeiraw42@hotmail.com>
2024-01-05 02:02:38 -0300

    Separating options into a function

--- src/Components/ConfigurationPanel.js
+++ src/Components/ConfigurationPanel.js
@@ -11,6 +11,30 @@ export default function ConfigurationPanel({state, setState, children}) {
         "Scale Generator": "Scale Generator",
     }
 
+    const ModeOption = ({ option }) => {
+        const options = {
+            "Random Generator": (
+                <>
+                    <Slider title="Speed in Seconds" state={state} setState={setState} />
+                    <Toggle title="Sharp Chords" state={state} setState={setState} />
+                    <Toggle title="Flat Chords" state={state} setState={setState} />
+                </>
+            ),
+            "Scale Generator": (
+                <>
+                    <SelectMenu title="Choose the Key" options={keys} state={state} setState={setState} />
+                    {state["Choose the Key"] && state["Choose the Key"] !== "Random" ? (
+                        <>
+                            <Slider title="Key Change Speed" state={state} setState={setState} />
+                            <SelectMenu title="Choose the chords scale" state={state} setState={setState} options={scales} />
+                        </>
+                    ):("")}
+                </>
+            ),
+        }
+        return options[option]
+    }
+
     return (
         <div className='bg-zinc-800 rounded-lg p-2 my-4 mx-auto w-[300px] md:w-[700px]'>
             <div className='rounded md:p-2 text-sm'>
@@ -22,23 +46,7 @@ export default function ConfigurationPanel({state, setState, children}) {
                     <span className="flex-shrink mx-4 text-gray-400">Configuration</span>
                     <div className="flex-grow border-t border-gray-400"></div>
                 </div>
-                {state["Mode"] === "Random Generator" ? 
-                    <>
-                        <Slider title="Speed in Seconds" state={state} setState={setState} />
-                        <Toggle title="Sharp Chords" state={state} setState={setState} />
-                        <Toggle title="Flat Chords" state={state} setState={setState} />
-                    </>
-                : 
-                    <>
-                        <SelectMenu title="Choose the Key" options={keys} state={state} setState={setState} />
-                        {state["Choose the Key"] && state["Choose the Key"] !== "Random" ? (
-                            <>
-                                <Slider title="Key Change Speed" state={state} setState={setState} />
-                                <SelectMenu title="Choose the chords scale" state={state} setState={setState} options={scales} />
-                            </>
-                        ):("")}
-                    </>
-                }
+                <ModeOption option={state["Mode"]}/>
             </div>
         </div>
     );
