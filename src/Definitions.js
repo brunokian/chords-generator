28b7879607d3e587b79d13c9c8e3121b7c023a4b
Wes <teixeiraw42@hotmail.com>
2024-01-09 00:20:30 -0300

    Adding more definitions

--- src/Definitions.js
+++ src/Definitions.js
@@ -3,6 +3,9 @@ let naturalNotes = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
 let sharpNotes = ['C#', 'D#', 'F#', 'G#', 'A#']
 let flatNotes = ['Db', 'Eb', 'Gb', 'Ab', 'Bb']
 let allNotes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+let relatives = ['2b', '2', '3b', '3', '4', '5b', '5', '5#', '6b', '6', '7b', '7', '7#', '9b', '9', '11', '13b', '13']
+let chordTypes = ['m', '4', '5-', '5+', '6', '7', '7M', '9b', '9', '11', '11+', '13-', '13', '°']
+
 let scales = {
     "Natural":  ["1", "2", "b3", "4", "5", "b6", "b7"],
     "Bepop":    ["1", "2", "3", "4", "5", "6", "b7", "7"],
@@ -20,5 +23,7 @@ export {
     flatNotes,
     allNotes,
     scales,
-    voicings
+    voicings,
+    relatives,
+    chordTypes
 }
